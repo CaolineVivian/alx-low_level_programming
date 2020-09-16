ec09a195cf20ba79956acdc534e084f7f8f51ef7
CaolineVivian <anyangov49@gmail.com>
2020-09-16 02:52:45 +0000

    print the size of computers

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+# include <stdio.h>
+
+int main (void)
+{
+	char a;
+	int b;
+	float j;
+
+	printf("Size of a char: byte(s)\n");
+	printf("Size of an int: byte(s)\n");
+	printf("Size of a long int: byte(s)\n");
+	printf("Size of alon long int: byte(s)\n");
+	printf("Size of a float: byte(s)\n");
+
+	return(0);
+}
