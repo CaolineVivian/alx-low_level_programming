673d0c5dd7ee405da18f88af778d0bb6ee60e348
CaolineVivian <anyangov49@gmail.com>
2020-09-16 20:28:02 +0000

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ *main - starts the code
+ *Description: prints "Programming is like building a multilingual puzzle
+ *Return: int
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
-
-	return(0);
-
+	return (0);
 }
