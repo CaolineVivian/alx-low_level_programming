7766611819d4aacf68273e43c429a064a7aad937
CaolineVivian <anyangov49@gmail.com>
2020-09-17 21:16:44 +0000

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - code execution
+ * Description: print single digits numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	/*print numbers*/
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
