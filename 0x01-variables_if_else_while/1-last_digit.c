081aa886fa25f44108d90dd78a7676ebb98f8944
CaolineVivian <anyangov49@gmail.com>
2020-09-17 14:01:24 +0000

    check if random number is positive or negative

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdtime.h>
+#include <time.h>
 #include <stdio.h>
 /**
  * main - start code execution
@@ -22,7 +22,7 @@ int main(void)
 	{
 		printf("Last digit of %d is and is 0\n", n);
 	}
-	else if (n < 6) && (n != 0)
+	else if (n < 6 && n != 0)
 	{
 		printf("Last digit of %d is and is less than 6 and not 0\n", n);
 	}
