0577b480d6b1832400e808022db249735a335595
CaolineVivian <anyangov49@gmail.com>
2020-09-18 10:08:03 +0000

    is random number positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,23 +8,15 @@
  */
 int main(void)
 {
-	/*integer value*/
 	int n;
-	/*the random number function*/
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/*if statement*/
 	if (n > 0)
-	{
 		printf("%i is positve\n", n);
-	}
 	else if (n == 0)
-	{
 		printf("%i is zero\n", n);
-	}
 	else
-	{
 		printf("%i is negative\n", n);
-	}
 	return (0);
 }
