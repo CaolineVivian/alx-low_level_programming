63c441eafac41ac7403aa9b38f79cd4d3d02c58a
CaolineVivian <anyangov49@gmail.com>
2020-09-22 13:26:01 +0000

    positve or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * main - tests function that print if integer is positive or ngative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
