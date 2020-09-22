cda3caff9ad8e4d014e61f20a3fb92328056a264
CaolineVivian <anyangov49@gmail.com>
2020-09-22 10:19:49 +0000

    header file

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,3 +1,23 @@
-int _putchar(char c);
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* _putchar.c */
+int _putchar(char);
+
+/* 1-alphabet.c */
 void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
 void print_alphabet_x10(void);
+
+/* 3-islower.c */
+int _islower(int);
+
+/* 4-alpha.c */
+int _isalpha(int);
+
+/* 5-sign.c */
+int print_sign(int);
+
+/* 6-abs.c */
+int _abs(int);
