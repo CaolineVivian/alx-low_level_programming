e7cfed1ecda655f83330b7ca13b5757fea163604
CaolineVivian <anyangov49@gmail.com>
2020-09-22 10:30:53 +0000

    check for letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to print
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+
+	for ((c >= 65 && c <= 90) || (c >= 97 && c <=122))
+	{
+		return (1);
+	}
+	return (0);
+}
