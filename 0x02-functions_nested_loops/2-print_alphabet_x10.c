e89ba282848f3ad7c7ce4139d5554629a67058fd
CaolineVivian <anyangov49@gmail.com>
2020-09-22 09:41:30 +0000

    check print alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 /**
- * main - code execution
- * Description: print lowercase alphabets 10 times
- * Return: 0
+ * print_alphabet_x10 - print lowercase alphabets 10 times
  */
 void print_alphabet_x10(void)
 {
 	int i = 48;
-	char ch;
+	char c;
 
 	while (i <= 57)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		c = 'a';
+		while (c <= 'z')
 		{
-			_putchar(ch);
-			ch++;
+			_putchar(c);
+			c++;
 		}
 		_putchar('\n');
 		i++;
