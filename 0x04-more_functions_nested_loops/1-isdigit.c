0f649b4b51ab9f362f1e098eb63fba89989d821c
CaolineVivian <anyangov49@gmail.com>
2020-09-24 10:17:39 +0000

    check if integer is digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * _isdigit - check for a digit 0 through 9
+ * @c: integer to be checked
+ *
+ * Return: 1 or 0
+ */
+int _isdigit(int c)
+{
+	int ch = 48;
+
+	while (ch <= 58)
+	{
+		if (ch == c)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+		ch++;
+	}
+	return (0);
+}
