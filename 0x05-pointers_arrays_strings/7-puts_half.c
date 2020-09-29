ad5ed9c0b497fd76b0a50f82086a1a90d38e5f8a
CaolineVivian <anyangov49@gmail.com>
2020-09-29 15:57:28 +0000

    print last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string variable
+ *
+ */
+void puts_half(char *str)
+{
+	int i, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	for (n = i / 2; str[n] != '\0'; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
+}
