1bd5ed93542e2e3db6f685a7f37cd8fcda80ad3f
CaolineVivian <anyangov49@gmail.com>
2020-10-01 23:24:30 +0000

    copy string from src to dest

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,6 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	{
-	}
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		if (i < n)
@@ -21,5 +18,6 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[i] = src[i];
 		}
 	}
+	dest[i] = '\0';
 	return (dest);
 }
