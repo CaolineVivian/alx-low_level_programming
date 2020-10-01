9cec1703262aa836623fe2c8d421c5bdb9f1a087
CaolineVivian <anyangov49@gmail.com>
2020-10-01 20:03:35 +0000

    header file

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -4,5 +4,7 @@
 int _putchar(char c);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *leet(char *str);
+char *rot13(char *str);
 
 #endif /* HOLBERTON_H */
