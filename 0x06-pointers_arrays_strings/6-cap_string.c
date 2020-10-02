b7e1f5f8bab76c2b2154143842d1ee27e3c9b9b6
CaolineVivian <anyangov49@gmail.com>
2020-10-02 00:20:28 +0000

    capitalizes all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes all word of a string
+ * @str: pointer to string variable
+ * Return: str
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 && str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] -= 32;
+		}
+		if (str[i] >= 97 && str[i] <= 122 &&
+		(str[i - 1] == 32 || str[i - 1] == ','
+		 || str[i - 1] == ';' || str[i - 1] == ','
+		 || str[i - 1] == '!' || str[i - 1] == '?'
+		 || str[i - 1] == '"' || str[i - 1] == '('
+		 || str[i - 1] == ')' || str[i - 1] == '{'
+		 || str[i - 1] == '}' || str[i - 1] == '\n'
+		 || str[i - 1] == '\t'))
+		{
+			str[i] -= 32;
+		}
+	}
+	return (str);
+}
