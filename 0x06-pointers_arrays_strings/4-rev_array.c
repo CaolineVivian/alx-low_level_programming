d8251d655006925bcc54eeb6b021682f198180a3
CaolineVivian <anyangov49@gmail.com>
2020-10-02 15:20:27 +0000

    printa array in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * reverse_array - reverse the contents of an array
+ * @a: pointer to array
+ * @n: number of elements in the array
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int forward, reverse;
+
+	for (forward = 0; a[forward] != '\0'; j++)
+	{
+	}
+
+	reverse = forward - 1;
+
+	for (reverse < n && reverse >= a[forward]; reverse--)
+	{
+		_putchar(a[reverse]);
+	}
+}
