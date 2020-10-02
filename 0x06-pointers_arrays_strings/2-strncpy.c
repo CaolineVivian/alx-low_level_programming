980eeb51774a5ed8995924ea758cd72c1180cc83
CaolineVivian <anyangov49@gmail.com>
2020-10-02 15:00:34 +0000

    copies two strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,9 +11,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int a;
 
-	for (a = 0; a < n && src[a] != '\0';)
+	for (a = 0; a < n && src[a] != '\0'; a++)
 	{
-		dest[i] = src[i];
+		dest[a] = src[a];
 	}
 	while (a < n)
 	{
