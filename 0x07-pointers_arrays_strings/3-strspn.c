6b67b4d282a2b81669f170f561d90480739b555f
CaolineVivian <anyangov49@gmail.com>
2020-10-05 14:24:58 +0000

    get length of prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -9,24 +9,21 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	int index;
-	int byte = 0;
+	int index, c;
 
-	while (*s)
+	for (c = 0; s[c] != '\0'; c++)
 	{
 		for (index = 0; accept[index]; index++)
 		{
-			if (*s == accept[index])
+			if (s[c] == accept[index])
 			{
-				byte++;
 				break;
 			}
 			else if (accept[index + 1] == '\0')
 			{
-				return (byte);
+				return (c);
 			}
 		}
-		s++
 	}
-	return (byte);
+	return (c);
 }
