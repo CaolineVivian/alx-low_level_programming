0634a7844115e3db6834d9d05c9b8d5e89e4c9f1
CaolineVivian <anyangov49@gmail.com>
2020-10-05 13:31:19 +0000

    get length of prefix substring

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _strspn - gets length of prefix substring
+ * @s: pointer to string
+ * @accept: pointer to substring
+ *
+ * Return: bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int index;
+	unsigned int c;
+
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		for (index = 0; accept[index] != '\0'; index++)
+		{
+			if (s[c] == accept[index])
+			{
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (c);
+			}
+		}
+	}
+	return (c);
+}
