018b8d722ac4f838c142b0deea8e579a9701d5e6
CaolineVivian <anyangov49@gmail.com>
2020-10-19 16:13:30 +0300

    dog headef file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - new type struct dog
+ * @name: variable of type char
+ * @age: variable of type float
+ * @owner: variable of type char
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* ifndef DOG_H*/
